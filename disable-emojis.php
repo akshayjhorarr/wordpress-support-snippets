9863d93778bf142e1f145ad8b8a5aa8bb05fc570
akshayjhorarr <akshayjhorarr@gmail.com>
2026-05-14 06:49:30 +0530

    Add explanation for disable emojis snippet

--- disable-emojis.php
+++ disable-emojis.php
@@ -1,3 +1,16 @@
+/*
+Problem:
+WordPress emoji scripts were loading unnecessarily.
+
+Solution:
+Disabled emoji scripts and styles to reduce extra requests.
+
+Result:
+Improved website performance and reduced unused assets.
+*/
+
+
+
 <?php
 remove_action('wp_head', 'print_emoji_detection_script', 7);
 remove_action('wp_print_styles', 'print_emoji_styles');
