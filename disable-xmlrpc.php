f18970029a09f7384caf4d6280fce7d67b40212e
akshayjhorarr <akshayjhorarr@gmail.com>
2026-05-14 06:50:50 +0530

    Add explanation for XML-RPC security snippet

--- disable-xmlrpc.php
+++ disable-xmlrpc.php
@@ -1,2 +1,14 @@
+/*
+Problem:
+XML-RPC can be abused for brute-force attacks and unnecessary remote access.
+
+Solution:
+Disabled XML-RPC functionality in WordPress.
+
+Result:
+Improved WordPress security and reduced attack surface.
+*/
+
+
 <?php
 add_filter('xmlrpc_enabled', '__return_false');
